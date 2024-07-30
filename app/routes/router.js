d2d2665392e9cc6d15adbda800b7c988c344fc1e
Ana Julia <tracoperfeito2024@outlook.com>
2024-07-30 15:52:05 -0300

    atualizacao 30/07

--- app/routes/router.js
+++ app/routes/router.js
@@ -3,13 +3,36 @@ var router = express.Router();
 
 
 router.get("/", function (req, res) {
+    res.render('pages/index')
  
 });
 
-router.post("???", function (req, res) {
 
-
+router.get("/contratar", function (req, res) {
+    res.render('pages/contratar')
+ 
 });
+
+
+router.get("/quemsomos", function (req, res) {
+    res.render('pages/quemsomos')
+ 
+});
+
+
+router.get("/login", function (req, res) {
+    res.render('pages/login');
+
+ 
+});
+
+
+router.get("/cadastro", function (req, res) {
+    res.render('pages/cadastro');
+ 
+});
+
+
 
 
 
