72035b9105c3db3569bd1b1888070e42c7569f08
Ana Julia <tracoperfeito2024@outloook.com>
2024-10-28 23:33:59 -0300

    ana julia

--- app/routes/router.js
+++ app/routes/router.js
@@ -253,8 +253,13 @@ router.get("/cadastro", function (req, res) { //cadastrar
 router.post( //validações cadastrar
     "/cadastro",
 
-    body("nome").isLength({ min: 2, max: 50 }).withMessage('O nome deve ter de 3 a 50 caracteres.'),
-
+    body("nome").isLength({ min: 3, max: 50 }).withMessage('O nome deve ter de 3 a 50 caracteres.')
+      .custom(nome => {
+        if (/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/.test(nome)) {
+            throw new Error('O nome deve conter apenas letras.');
+        }
+        return true; 
+      }),
     body("usuario").isLength({ min: 6, max: 20 }).withMessage('O usuario deve ter de 6 a 20 caracteres.'),
 
     body("email").isEmail().withMessage('Insira um e-mail válido.'),
